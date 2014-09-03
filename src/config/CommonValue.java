cbb1e76c3d2c34da7733eb05c03c0795826a1c3f
Donal <donal.tong@gmail.com>
2014-09-03 10:05:25 +0800

    微信key

--- src/config/CommonValue.java
+++ src/config/CommonValue.java
@@ -13,8 +13,8 @@ import com.nostra13.universalimageloader.core.imageaware.ImageAware;
 import com.vikaa.wecontact.R;
 
 public class CommonValue {
-    public static final String APP_ID = "wx8b5b960fc0311f3e";
-    public static final String SECRET = "918774b3b42e14e42e84d67f54549a98";
+    public static final String APP_ID = "wxe0726da6ece79559";
+    public static final String SECRET = "e62df785b3030a27b634cc3d0b8967de";
 
     public static final String WMAppID = "1-20178-d86ca85fa3bf8db970f11a9fb89f49af";
     public static final String WMSECRET = "e468cf291e106ac6c9460395b8b105ab";
